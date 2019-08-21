8c31ba9f4612441b96a98da1cf09251946a8f58e
Vitor Veras <vitorverasm@gmail.com>
2019-08-20 23:59:10 -0300

    Formata saida

--- itemB.m
+++ itemB.m
@@ -33,7 +33,17 @@ sense = -1;
 param.msglev = 1;
 
 [xopt, fopt, status] = glpk (c, A, b, lb, ub, ctype, vartype, sense, param);
+xfinal = reshape(xopt, 5, 8)';
 
-reshape(xopt, 5, 8)', fopt, status;
+fprintf(1,'\n           S  T  Q  Q  S\n')
+fprintf(1,'\nPaloma     %s', num2str(xfinal(1,:)))
+fprintf(1,'\nLeonardo   %s', num2str(xfinal(2,:)))
+fprintf(1,'\nAmanda     %s', num2str(xfinal(3,:)))
+fprintf(1,'\nJoão Paulo %s', num2str(xfinal(4,:)))
+fprintf(1,'\nRégis      %s', num2str(xfinal(5,:)))
+fprintf(1,'\nGustavo    %s', num2str(xfinal(6,:)))
+fprintf(1,'\nJéssica    %s', num2str(xfinal(7,:)))
+fprintf(1,'\nRenan      %s\n', num2str(xfinal(8,:)))
 
+fprintf(1,'\nCusto: %s\n', num2str(fopt))
 
